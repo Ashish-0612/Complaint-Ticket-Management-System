5e9ba6438a9f5d2a7745efca8a1e1646ea7dce3e
Ashish Yadav <ashishyadav0962850@gmail.com>
2026-08-04 00:48:25 +0530

    fix mobile sidebar drawer

--- ctms-frontend/src/pages/User/Dashboard.jsx
+++ ctms-frontend/src/pages/User/Dashboard.jsx
@@ -228,18 +228,17 @@ const Dashboard = () => {
 
       {/* Mobile drawer */}
       {mobileMenuOpen && (
-        <div
-          className="fixed inset-0 z-50 bg-black/50 md:hidden"
-          onClick={() => setMobileMenuOpen(false)}
-        />
+        <>
+          <div
+            className="fixed inset-0 z-50 bg-black/50"
+            onClick={() => setMobileMenuOpen(false)}
+          />
+
+          <aside className="fixed top-0 left-0 z-[9999] h-screen w-64 bg-gray-900 flex flex-col">
+            {SidebarInner}
+          </aside>
+        </>
       )}
-      <aside
-        className={`md:hidden fixed top-0 left-0 z-50 h-screen w-64 bg-gray-900 flex flex-col transition-transform duration-300 ease-in-out ${
-          mobileMenuOpen ? "translate-x-0" : "-translate-x-full"
-        }`}
-      >
-        {SidebarInner}
-      </aside>
 
       {/* ========== MAIN ========== */}
       <div className="flex-1 flex flex-col overflow-hidden pt-14 md:pt-0">
