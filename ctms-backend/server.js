b9ae7df1a7bc159b58408a52499286c897e39dce
Ashish Yadav <ashishyadav0962850@gmail.com>
2026-08-03 15:14:42 +0530

    fix cors and render proxy issue

--- ctms-backend/server.js
+++ ctms-backend/server.js
@@ -56,22 +56,30 @@ app.set("trust proxy", 1);
 
 app.use(helmet());
 
-// ================= CORS =================
-
 app.use(
   cors({
     origin: [
       "http://localhost:5173",
       "http://127.0.0.1:5173",
-      "https://complaint-ticket-management-system-rho.vercel.app",
+      "https://complaint-ticket-management-system-rho.vercel.app"
     ],
     credentials: true,
-    methods: ["GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"],
-    allowedHeaders: ["Content-Type", "Authorization"],
-  }),
+    methods: [
+      "GET",
+      "POST",
+      "PUT",
+      "PATCH",
+      "DELETE",
+      "OPTIONS"
+    ],
+    allowedHeaders: [
+      "Content-Type",
+      "Authorization"
+    ]
+  })
 );
 
-app.options("*", cors());
+
 
 // ================= STATIC FILES =================
 
