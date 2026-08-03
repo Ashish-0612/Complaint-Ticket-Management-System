901ecc007855dddf8a6156b9c4c36cd6cd37a34a
Ashish Yadav <ashishyadav0962850@gmail.com>
2026-08-04 00:40:35 +0530

    fix cors vercel domain

--- ctms-backend/server.js
+++ ctms-backend/server.js
@@ -61,22 +61,13 @@ app.use(
     origin: [
       "http://localhost:5173",
       "http://127.0.0.1:5173",
-      "https://complaint-ticket-management-system-rho.vercel.app"
+      "https://complaint-ticket-management-system-kiy9-7a6ohvoaa-ashu-f213.vercel.app",
+      "https://complaint-ticket-management-system-rho.vercel.app",
     ],
     credentials: true,
-    methods: [
-      "GET",
-      "POST",
-      "PUT",
-      "PATCH",
-      "DELETE",
-      "OPTIONS"
-    ],
-    allowedHeaders: [
-      "Content-Type",
-      "Authorization"
-    ]
-  })
+    methods: ["GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"],
+    allowedHeaders: ["Content-Type", "Authorization"],
+  }),
 );
 
 
